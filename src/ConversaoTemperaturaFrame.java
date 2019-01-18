73933b6f025c018e655c0c49930657387daadc76
joao <joao182cx@gmail.com>
2019-01-18 17:09:49 -0200

    Criação de um objeto KeyAdapter para ouvir o evento de digitação do teclado, que ao acontecer esse evento, pega o texto de JTextField, calcula a temperatura em celsius e atualiza um JLabel com um valor.

--- src/ConversaoTemperaturaFrame.java
+++ src/ConversaoTemperaturaFrame.java
@@ -1,5 +1,7 @@
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -22,12 +24,32 @@ public class ConversaoTemperaturaFrame extends JFrame {
 		
 		labelFahrenheit = new JLabel("Fahrenheit:");
 		fahrenheit = new JTextField(3);
-		celsius = new JLabel("Celsius");
+		celsius = new JLabel("Celsius:");
 		panelFahrenheit = new JPanel();
 		panelCelsius = new JPanel();
 		
 		panelFahrenheit.setLayout(new FlowLayout());
 		panelCelsius.setLayout(new FlowLayout());
+		
+		fahrenheit.addKeyListener(
+				
+				new KeyAdapter() {
+					
+					@Override
+					public void keyReleased(KeyEvent event) {
+						
+						final double temp = 5.0/9.0 * (Double.parseDouble(fahrenheit.getText()) - 32);
+						
+						System.out.println(Integer.parseInt(fahrenheit.getText()));
+						
+						
+						celsius.setText(String.format("Celsius: %.1f", temp));
+						
+					}
+					
+				}
+				
+				);
 		
 		panelFahrenheit.add(labelFahrenheit);
 		panelFahrenheit.add(fahrenheit);
